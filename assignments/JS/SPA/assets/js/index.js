73aaea638d89183dc9c2f586d5b1836968bde8eb
Sandun-Induranga <sandutharu40@gmail.com>
2022-09-19 22:30:11 +0530

    Add click on action to button customer

--- assignments/JS/SPA/assets/js/index.js
+++ assignments/JS/SPA/assets/js/index.js
@@ -1,1 +1,13 @@
+$('#btnCustomer').click(function () {
+    $('#btnDashboard a').css('color','#6c757d')
+    $('#btnCustomer a').css('color','black')
+    $('#btnItems a').css('color','#6c757d')
+    $('#btnOrders a').css('color','#6c757d')
+    $('#btnOrderDetails a').css('color','#6c757d')
 
+    $('#dashboard').css('display','none !important')
+    $('#customer').css('display','block !important')
+    $('#item').css('display','none !important')
+    $('#place-order').css('display','none !important')
+    $('#order-details').css('display','none !important')
+})
