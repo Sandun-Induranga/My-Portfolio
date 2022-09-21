836135fba2b9cfeba07613c0313a6d002cd541ba
Sandun-Induranga <sandutharu40@gmail.com>
2022-09-21 15:40:03 +0530

    Dark mode applied for background

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
 
 // dark mode
 $("#dark").click(function () {
+
+    // top bar
     $('.top-bar').css('background','#2B2D42')
     $('.top-bar a').css('color','white')
+
+    $('.background').remove()
+    $('body').css('background','#2B2D42')
 })
