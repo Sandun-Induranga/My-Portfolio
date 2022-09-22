6c08825a8db7c83cc072adac5e978875cdc00c76
Sandun-Induranga <sandutharu40@gmail.com>
2022-09-22 15:11:10 +0530

    Content Updated

--- assignments/JS/SPA/assets/js/index.js
+++ assignments/JS/SPA/assets/js/index.js
@@ -1,3 +1,16 @@
+$(document).ready(function () {
+    $('#btnDashboard a').css('color', 'black')
+    $('#btnCustomer a').css('color', '#6c757d')
+    $('#btnItems a').css('color', '#6c757d')
+    $('#btnOrders a').css('color', '#6c757d')
+    $('#btnOrderDetails a').css('color', '#6c757d')
+
+    $('#dashboard').attr('style', 'display : block !important')
+    $('#customer').attr('style', 'display : none !important')
+    $('#item').attr('style', 'display : none !important')
+    $('#place-order').attr('style', 'display : none !important')
+    $('#order-details').attr('style', 'display : none !important')
+})
 $('#btnDashboard').click(function () {
     $('#btnDashboard a').css('color', 'black')
     $('#btnCustomer a').css('color', '#6c757d')
