dcd214a9eeac7d25251582fee191b49278768420
Sandun-Induranga <sandutharu40@gmail.com>
2022-09-24 20:35:49 +0530

    Set function for clear button

--- assignments/JS/Calculator/assets/js/index.js
+++ assignments/JS/Calculator/assets/js/index.js
@@ -1,7 +1,15 @@
 let number = "";
+
+/* Get input from numbers and symbols */
 for (let i = 0; i < 17; i++) {
     $("#btn"+i).click(function () {
-        number += $("#btn"+i).text()
-        $(".display input").val(number)
+        number += $("#btn"+i).text();
+        $(".display input").val(number);
     });
 }
+
+/* Clear display */
+$("#btnClear").click(function () {
+    number = "";
+    $(".display input").val("");
+});
