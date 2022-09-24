980cf748d4e76428fc610c54383a1fc1620ee481
Sandun-Induranga <sandutharu40@gmail.com>
2022-09-24 23:39:00 +0530

    Get + button

--- assignments/JS/Calculator/assets/js/index.js
+++ assignments/JS/Calculator/assets/js/index.js
@@ -1,9 +1,17 @@
 let number = "";
+let firstNumber = 0;
+let lastNumber = 0;
 
 /* Get input from numbers and symbols */
 for (let i = 0; i < 17; i++) {
-    $("#btn"+i).click(function () {
-        number += $("#btn"+i).text();
+    $("#btn" + i).click(function () {
+        let pressedBtn = $("#btn" + i).text();
+        if (pressedBtn==="+"){
+            firstNumber = number;
+            number = ""
+            pressedBtn = ""
+        }
+        number += pressedBtn;
         $(".display input").val(number);
     });
 }
