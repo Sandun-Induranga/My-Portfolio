ffe2fc1e1323689d398871f98a08c9cc5d1cae4c
Sandun-Induranga <sandutharu40@gmail.com>
2022-09-24 23:49:39 +0530

    Content Updated

--- assignments/JS/Calculator/assets/js/index.js
+++ assignments/JS/Calculator/assets/js/index.js
@@ -1,19 +1,37 @@
 let number = "";
 let firstNumber = 0;
 let lastNumber = 0;
+let operator = ""
 
 /* Get input from numbers and symbols */
 for (let i = 0; i < 17; i++) {
     $("#btn" + i).click(function () {
         let pressedBtn = $("#btn" + i).text();
-        if (pressedBtn==="+"){
+        if (pressedBtn === "+") {
             firstNumber = number;
             number = ""
             pressedBtn = ""
+            operator = "+"
+            $(".display input").val(number);
+        } else if (pressedBtn === "=") {
+            lastNumber = number;
+            number = ""
+            pressedBtn = ""
+            let ans = operations(operator, firstNumber, lastNumber);
+            $(".display input").val(ans);
+        }else {
+            number += pressedBtn;
+            $(".display input").val(number);
         }
-        number += pressedBtn;
-        $(".display input").val(number);
     });
+}
+
+function operations(operator, firstNum, lastNum) {
+    switch (operator) {
+        case "+":
+            return firstNum + lastNum;
+            break;
+    }
 }
 
 /* Clear display */
