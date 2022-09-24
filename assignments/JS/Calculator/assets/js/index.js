324d534eb9a3fbbefcc5165f10aace85474ccc80
Sandun-Induranga <sandutharu40@gmail.com>
2022-09-24 16:11:20 +0530

    Created click function

--- assignments/JS/Calculator/assets/js/index.js
+++ assignments/JS/Calculator/assets/js/index.js
@@ -1,4 +1,9 @@
-let buttons = $(".keys").text();
-for (const buttonsKey in buttons) {
-    console.log(buttons[buttonsKey])
+const keys = [];
+
+for (let i = 0; i < 17; i++) {
+    keys[i] = $('.keys '.indexOf(i));
+    let key = keys[i].text()
+    document.getElementsByClassName("keys")[i].addEventListener('click', function () {
+        console.log(document.getElementsByClassName("keys")[i].textContent);
+    });
 }
