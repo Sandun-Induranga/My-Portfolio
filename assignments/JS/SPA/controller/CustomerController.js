1989032485bfd3244ade6106fc5fa88d503312c7
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-08 15:36:10 +0530

    Created function deleteCustomer

--- assignments/JS/SPA/controller/CustomerController.js
+++ assignments/JS/SPA/controller/CustomerController.js
@@ -1,3 +1,4 @@
+// Button Save On Action
 $("#btnSaveCustomer").on("click", function () {
     saveCustomer();
 });
@@ -8,19 +9,17 @@ function saveCustomer() {
     let address = $("#txtAddress").val();
     let salary = $("#txtSalary").val();
 
-    if ($("#btnSaveCustomer").text()=="Save"){
+    if ($("#btnSaveCustomer").text() == "Save") {
         var customer = setCustomer(customerId, name, address, salary);
         customerDB.push(customer);
-    }else {
+    } else {
         updateCustomer(customerId, name, address, salary);
     }
 
     loadAllCustomers();
-    bindEditEvent();
-
 }
 
-// load all customers
+// Load all customers
 function loadAllCustomers() {
 
     $("#tblCustomer > tbody").empty();
@@ -29,25 +28,36 @@ function loadAllCustomers() {
         $("#tblCustomer > tbody").append(
             `<tr><td>${customer.cusId}</td><td>${customer.cusName}</td><td>${customer.cusAddress}</td><td>${customer.cusSalary}</td><td><i class="bi bi-pencil-fill text-success me-4" data-bs-toggle="modal" data-bs-target="#staticBackdrop"></i><i class="bi bi-trash text-danger"></i></td></tr>`
         );
+        bindEditEvent();
+        bindDeleteEvent();
     }
 }
 
-// edit button on action
+// Edit button on action
 function bindEditEvent() {
+
     $(".bi-pencil-fill").on("click", function () {
-            var id = $(this).parent().parent().children(":eq(0)").text();
+        var id = $(this).parent().parent().children(":eq(0)").text();
 
-            var name = $(this).parent().parent().children(":eq(1)").text();
+        var name = $(this).parent().parent().children(":eq(1)").text();
 
-            var address = $(this).parent().parent().children(":eq(2)").text();
+        var address = $(this).parent().parent().children(":eq(2)").text();
 
-            var salary = $(this).parent().parent().children(":eq(3)").text();
+        var salary = $(this).parent().parent().children(":eq(3)").text();
 
-            setCustomerTextFields(id, name, address, salary);
+        setCustomerTextFields(id, name, address, salary);
+        loadAllCustomers();
+        $("#btnSaveCustomer").text("Update");
+    });
+}
 
-            $("#btnSaveCustomer").text("Update");
-        }
-    )
+function bindDeleteEvent() {
+    // Delete button on action
+    $(".bi-trash").on("click", function () {
+        var cusId = $(this).parent().parent().children(":eq(0)").text();
+        deleteCustomer(cusId);
+        loadAllCustomers();
+    });
 }
 
 function setCustomerTextFields(id, name, address, salary) {
@@ -74,10 +84,19 @@ function updateCustomer(customerId, name, address, salary) {
         customer.cusAddress = address;
         customer.cusSalary = salary;
         console.log(customer);
-        loadAllCustomers();
         return true;
     } else {
         return false;
     }
+}
 
+function deleteCustomer(customerID) {
+    let customer = searchCustomer(customerID);
+    if (customer != null) {
+        let indexNumber = customerDB.indexOf(customer);
+        customerDB.splice(indexNumber, 1);
+        return true;
+    } else {
+        return false;
+    }
 }
