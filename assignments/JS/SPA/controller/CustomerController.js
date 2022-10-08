5606e94ab800bea9711f8ff2c707623627048fb8
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-08 23:34:08 +0530

    Created Regex

--- assignments/JS/SPA/controller/CustomerController.js
+++ assignments/JS/SPA/controller/CustomerController.js
@@ -120,3 +120,17 @@ function deleteCustomer(customerID) {
         return false;
     }
 }
+
+// customer regular expressions
+const cusIDRegEx = /^(C00-)[0-9]{1,3}$/;
+const cusNameRegEx = /^[A-z ]{5,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{7,}$/;
+const cusSalaryRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+let customerValidations = [];
+customerValidations.push({reg: cusIDRegEx, field: $('#txtCusId'),error:'Customer ID Pattern is Wrong : C00-001'});
+customerValidations.push({reg: cusNameRegEx, field: $('#txtCusName'),error:'Customer Name Pattern is Wrong : A-z 5-20'});
+customerValidations.push({reg: cusAddressRegEx, field: $('#txtAddress'),error:'Customer Address Pattern is Wrong : A-z 0-9 ,/'});
+customerValidations.push({reg: cusSalaryRegEx, field: $('#txtSalary'),error:'Customer Salary Pattern is Wrong : 100 or 100.00'});
+
+
