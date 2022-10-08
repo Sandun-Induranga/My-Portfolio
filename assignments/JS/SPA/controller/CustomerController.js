91de27efb1c5c7848515b25485a98c5d45cd11e0
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-08 11:04:37 +0530

    Created customer db

--- assignments/JS/SPA/controller/CustomerController.js
+++ assignments/JS/SPA/controller/CustomerController.js
@@ -9,4 +9,16 @@ function saveCustomer() {
     let salary = $("#txtSalary").val();
 
     console.log(setCustomer(customerId, name, address, salary));
+
+}
+
+function loadAllCustomers() {
+
+    $("#tblCustomer > tbody").empty();
+
+    for (let customer of customers) {
+        $("#tblCustomer > tbody").append(
+            `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.salary}</td><td><i class="bi bi-pencil-fill text-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="edit"></i><i class="bi bi-trash text-danger"></i></td></tr>`
+        );
+    }
 }
