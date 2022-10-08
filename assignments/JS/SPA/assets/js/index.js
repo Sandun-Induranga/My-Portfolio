c6dfb97c829ad4cca6f925fee994ea9fef6faed5
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-08 10:16:20 +0530

    Content Updated

--- assignments/JS/SPA/assets/js/index.js
+++ assignments/JS/SPA/assets/js/index.js
@@ -1,83 +1,83 @@
 $(document).ready(function () {
-    $('#btnDashboard a').css('color', 'black')
-    $('#btnCustomer a').css('color', '#6c757d')
-    $('#btnItems a').css('color', '#6c757d')
-    $('#btnOrders a').css('color', '#6c757d')
-    $('#btnOrderDetails a').css('color', '#6c757d')
+    $('#btnDashboard a').css('color', 'black');
+    $('#btnCustomer a').css('color', '#6c757d');
+    $('#btnItems a').css('color', '#6c757d');
+    $('#btnOrders a').css('color', '#6c757d');
+    $('#btnOrderDetails a').css('color', '#6c757d');
 
-    $('#dashboard').attr('style', 'display : block !important')
-    $('#customer').attr('style', 'display : none !important')
-    $('#item').attr('style', 'display : none !important')
-    $('#place-order').attr('style', 'display : none !important')
-    $('#order-details').attr('style', 'display : none !important')
-})
+    $('#dashboard').attr('style', 'display : block !important');
+    $('#customer').attr('style', 'display : none !important');
+    $('#item').attr('style', 'display : none !important');
+    $('#place-order').attr('style', 'display : none !important');
+    $('#order-details').attr('style', 'display : none !important');
+});
 
 $('#btnDashboard').click(function () {
-    $('#btnDashboard a').css('color', 'black')
-    $('#btnCustomer a').css('color', '#6c757d')
-    $('#btnItems a').css('color', '#6c757d')
-    $('#btnOrders a').css('color', '#6c757d')
-    $('#btnOrderDetails a').css('color', '#6c757d')
+    $('#btnDashboard a').css('color', 'black');
+    $('#btnCustomer a').css('color', '#6c757d');
+    $('#btnItems a').css('color', '#6c757d');
+    $('#btnOrders a').css('color', '#6c757d');
+    $('#btnOrderDetails a').css('color', '#6c757d');
 
-    $('#dashboard').attr('style', 'display : block !important')
-    $('#customer').attr('style', 'display : none !important')
-    $('#item').attr('style', 'display : none !important')
-    $('#place-order').attr('style', 'display : none !important')
-    $('#order-details').attr('style', 'display : none !important')
-})
+    $('#dashboard').attr('style', 'display : block !important');
+    $('#customer').attr('style', 'display : none !important');
+    $('#item').attr('style', 'display : none !important');
+    $('#place-order').attr('style', 'display : none !important');
+    $('#order-details').attr('style', 'display : none !important');
+});
 
 $('#btnCustomer').click(function () {
-    $('#btnDashboard a').css('color', '#6c757d')
-    $('#btnCustomer a').css('color', 'black')
-    $('#btnItems a').css('color', '#6c757d')
-    $('#btnOrders a').css('color', '#6c757d')
-    $('#btnOrderDetails a').css('color', '#6c757d')
+    $('#btnDashboard a').css('color', '#6c757d');
+    $('#btnCustomer a').css('color', 'black');
+    $('#btnItems a').css('color', '#6c757d');
+    $('#btnOrders a').css('color', '#6c757d');
+    $('#btnOrderDetails a').css('color', '#6c757d');
 
-    $('#dashboard').attr('style', 'display : none !important')
-    $('#customer').attr('style', 'display : flex !important')
-    $('#item').attr('style', 'display : none !important')
-    $('#place-order').attr('style', 'display : none !important')
-    $('#order-details').attr('style', 'display : none !important')
-})
+    $('#dashboard').attr('style', 'display : none !important');
+    $('#customer').attr('style', 'display : flex !important');
+    $('#item').attr('style', 'display : none !important');
+    $('#place-order').attr('style', 'display : none !important');
+    $('#order-details').attr('style', 'display : none !important');
+});
 
 $('#btnItems').click(function () {
-    $('#btnDashboard a').css('color', '#6c757d')
-    $('#btnCustomer a').css('color', '#6c757d')
-    $('#btnItems a').css('color', 'black')
-    $('#btnOrders a').css('color', '#6c757d')
-    $('#btnOrderDetails a').css('color', '#6c757d')
+    $('#btnDashboard a').css('color', '#6c757d');
+    $('#btnCustomer a').css('color', '#6c757d');
+    $('#btnItems a').css('color', 'black');
+    $('#btnOrders a').css('color', '#6c757d');
+    $('#btnOrderDetails a').css('color', '#6c757d');
 
-    $('#dashboard').attr('style', 'display : none !important')
-    $('#customer').attr('style', 'display : none !important')
-    $('#item').attr('style', 'display : flex !important')
-    $('#place-order').attr('style', 'display : none !important')
-    $('#order-details').attr('style', 'display : none !important')
-})
+    $('#dashboard').attr('style', 'display : none !important');
+    $('#customer').attr('style', 'display : none !important');
+    $('#item').attr('style', 'display : flex !important');
+    $('#place-order').attr('style', 'display : none !important');
+    $('#order-details').attr('style', 'display : none !important');
+});
 
 $('#btnOrders').click(function () {
-    $('#btnDashboard a').css('color', '#6c757d')
-    $('#btnCustomer a').css('color', '#6c757d')
-    $('#btnItems a').css('color', '#6c757d')
-    $('#btnOrders a').css('color', 'black')
-    $('#btnOrderDetails a').css('color', '#6c757d')
+    $('#btnDashboard a').css('color', '#6c757d');
+    $('#btnCustomer a').css('color', '#6c757d');
+    $('#btnItems a').css('color', '#6c757d');
+    $('#btnOrders a').css('color', 'black');
+    $('#btnOrderDetails a').css('color', '#6c757d');
 
-    $('#dashboard').attr('style', 'display : none !important')
-    $('#customer').attr('style', 'display : none !important')
-    $('#item').attr('style', 'display : none !important')
-    $('#place-order').attr('style', 'block : none !important')
-    $('#order-details').attr('style', 'display : none !important')
-})
+    $('#dashboard').attr('style', 'display : none !important');
+    $('#customer').attr('style', 'display : none !important');
+    $('#item').attr('style', 'display : none !important');
+    $('#place-order').attr('style', 'block : none !important');
+    $('#order-details').attr('style', 'display : none !important');
+});
 
 $('#btnOrderDetails').click(function () {
-    $('#btnDashboard a').css('color', '#6c757d')
-    $('#btnCustomer a').css('color', '#6c757d')
-    $('#btnItems a').css('color', '#6c757d')
-    $('#btnOrders a').css('color', '#6c757d')
-    $('#btnOrderDetails a').css('color', 'black')
+    $('#btnDashboard a').css('color', '#6c757d');
+    $('#btnCustomer a').css('color', '#6c757d');
+    $('#btnItems a').css('color', '#6c757d');
+    $('#btnOrders a').css('color', '#6c757d');
+    $('#btnOrderDetails a').css('color', 'black');
 
-    $('#dashboard').attr('style', 'display : none !important')
-    $('#customer').attr('style', 'display : none !important')
-    $('#item').attr('style', 'display : none !important')
-    $('#place-order').attr('style', 'display : none !important')
-    $('#order-details').attr('style', 'display : flex !important')
-})
+    $('#dashboard').attr('style', 'display : none !important');
+    $('#customer').attr('style', 'display : none !important');
+    $('#item').attr('style', 'display : none !important');
+    $('#place-order').attr('style', 'display : none !important');
+    $('#order-details').attr('style', 'display : flex !important');
+});
