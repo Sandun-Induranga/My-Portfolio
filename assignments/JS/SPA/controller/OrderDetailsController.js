aacd5d9c375dfbda937fa0673f08984b1e220866
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-12 22:31:43 +0530

    Created method loadAllOrders

--- assignments/JS/SPA/controller/OrderDetailsController.js
+++ assignments/JS/SPA/controller/OrderDetailsController.js
@@ -1,1 +1,17 @@
+$(function () {
+    $("form").submit(function () {
+        return false;
+    });
 
+    loadAllOrders();
+});
+
+function loadAllOrders() {
+    $("#tblCustomer > tbody").empty();
+
+    for (let order of ordersDB) {
+        $("#tblCustomer > tbody").append(
+            `<tr><td>${order.orderId}</td><td>${order.customerId}</td><td>${order.customerName}</td><td>${order.date}</td><td>${order.total}</td><td>${order.discount}</td><td>${order.amount}</td><td><i class="bi bi-trash text-danger customer-deletes"></i></td></tr>`
+        );
+    }
+}
