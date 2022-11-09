83249ffaa5b3f2c4cc61f7922c44e2106c15ca8a
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-10 00:07:03 +0530

    Content Updated

--- assignments/JS/Que/script.js
+++ assignments/JS/Que/script.js
@@ -1,1 +1,15 @@
+let imgSrc = "../img/man.gif"
+let queData = {
+    men:[
+        {letter: "A", img: imgSrc},
+        {letter: "B", img: imgSrc},
+        {letter: "C", img: imgSrc},
+        {letter: "D", img: imgSrc},
+        {letter: "E", img: imgSrc},
+        {letter: "F", img: imgSrc}
+    ]
+}
 
+function renderQue() {
+
+}
