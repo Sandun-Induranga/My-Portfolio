f5e2359b1b125b22d2f6d5948c630cf342643436
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-10 00:57:03 +0530

    Added Audio

--- assignments/JS/Knight/assets/js/script.js
+++ assignments/JS/Knight/assets/js/script.js
@@ -2,7 +2,7 @@ var initArray = [];
 var leftColors = ['white', 'white', 'white', 'white', 'white', 'white', '#d3c5c5', '#be7474', '#cd4a4a', '#b83030', '#c52525', '#dc1e1e'];
 var rightColors = ['white', 'white', 'white', 'white', 'white', 'white', '#dc1e1e', '#c52525', '#b83030', '#cd4a4a', '#be7474', '#d3c5c5'];
 
-// renderKnightRider();
+const audio = new Audio("assets/audio/KnightRider.mp3");
 
 function animateRight() {
     let firstValue = initArray.shift();
@@ -29,7 +29,13 @@ function animate() {
         animateRight();
     }
 }
-
+audio.addEventListener("canplaythrough", () => {
+    audio.play().catch(e => {
+        $("#btnStart").click(function () {
+                audio.play();
+        })
+    })
+});
 
 function renderKnightRider() {
     $("#container").empty();
