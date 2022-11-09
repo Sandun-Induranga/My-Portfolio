fbe01b5453ac852c39fadee8024230c32c6b8e58
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-10 00:21:38 +0530

    Que Completed

--- assignments/JS/Que/assets/js/script.js
+++ assignments/JS/Que/assets/js/script.js
@@ -1,4 +1,4 @@
-let imgSrc = "../img/man.gif";
+let imgSrc = "assets/img/man.gif";
 let queData = {
     men:[
         {letter: "A", img: imgSrc},
@@ -7,11 +7,21 @@ let queData = {
         {letter: "D", img: imgSrc},
         {letter: "E", img: imgSrc},
         {letter: "F", img: imgSrc}
-    ]
-}
-
-function renderQue() {
-    $("main> section:first-child").empty();
+    ],
+    nextMan:function (){
+        let lastMan= this.men.pop();
+        this.men.unshift(lastMan);
+    }
 }
 
 renderQue();
+
+function renderQue() {
+    $("main> section:first-child").empty();
+    for (let i = 0; i < queData.men.length; i++) {
+        $("main> section:first-child").append(`<div><img src="${queData.men[i].img}" alt="man"><h4>${queData.men[i].letter}</h4></div>`);
+    }
+    queData.nextMan();
+}
+
+setInterval(renderQue, 3000);
